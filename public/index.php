34bf405e5dc7e57d8c673da5217027f094818f51
dekito278 <aderizki68@gmail.com>
2020-07-06 23:57:40 -0700

    init

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../storyphoto/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,12 +35,12 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/storyphoto/bootstrap/app.php';
 
-// set the public path to this directory
-//$app->bind('path.public', function() {
-//return __DIR__;
-//});
+ //set the public path to this directory
+$app->bind('path.public', function() {
+return __DIR__;
+});
 
 /*
 |--------------------------------------------------------------------------
